c869511fd49cb5915cf62346e713dcb28ec3953d
unknown <raihanarif778@gmail.com>
2022-12-11 22:24:13 +0600

    margin issue solved

--- src/shared/Home/MyProjects/MyProjects.jsx
+++ src/shared/Home/MyProjects/MyProjects.jsx
@@ -11,7 +11,7 @@ const MyProjects = () => {
       <div className="mx-10">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
           {/* card-one */}
-          <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card w-96 mx-auto bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
               <img
                 src="https://i.ibb.co/zbySqqj/picture-Image.png"
@@ -35,7 +35,7 @@ const MyProjects = () => {
             </div>
           </div>
           {/* card-two */}
-          <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card w-96 mx-auto bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
               <img
                 src="https://i.ibb.co/8KwqLSS/bangle-dish.png"
@@ -57,7 +57,7 @@ const MyProjects = () => {
             </div>
           </div>
           {/* card-three */}
-          <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card w-96 mx-auto bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
               <img
                 src="https://i.ibb.co/ZhtsdVg/fun-quiz-hub.png"
