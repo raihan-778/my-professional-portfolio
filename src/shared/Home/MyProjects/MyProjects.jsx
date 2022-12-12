78a2b5fa0a1527ebb1ce6444a8b2386564adadc7
raihan-778 <raihanarif778@gmail.com>
2022-12-12 10:34:32 +0600

    responsive issue soleved

--- src/shared/Home/MyProjects/MyProjects.jsx
+++ src/shared/Home/MyProjects/MyProjects.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 
 const MyProjects = () => {
   return (
-    <div className=" my-5">
+    <div className="mx-5">
       <h2 className="mb-5">
         My Recent{" "}
         <span className="text-3xl mb-5 font-bold text-cyan-500">Projects</span>
       </h2>
-      <div className="mx-10">
+      <div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
           {/* card-one */}
-          <div className="card w-96 mx-auto bg-base-100 shadow-xl">
+          <div className="card w-100 mx-auto bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
               <img
                 src="https://i.ibb.co/zbySqqj/picture-Image.png"
@@ -24,8 +24,8 @@ const MyProjects = () => {
                 Used Product Re-sale App
               </h2>
               <p>
-                This a used producta re-selling platform.User authentication ,
-                Admin Panel, and payment system is intigrated in this app{" "}
+                This is a used producta re-selling platform.User authentication
+                , Admin Panel, and payment system is intigrated in this app.
               </p>
               <div className="card-actions">
                 <button className="btn btn-primary">
@@ -35,7 +35,7 @@ const MyProjects = () => {
             </div>
           </div>
           {/* card-two */}
-          <div className="card w-96 mx-auto bg-base-100 shadow-xl">
+          <div className="card w-100 mx-auto bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
               <img
                 src="https://i.ibb.co/8KwqLSS/bangle-dish.png"
@@ -46,8 +46,8 @@ const MyProjects = () => {
             <div className="card-body items-center text-center">
               <h2 className="card-title text-3xl font-bold">Food review App</h2>
               <p>
-                This a Food Review app . In this app user can review the food
-                service and add any food he want to tast.
+                This is a Food Review app . In this app user can review of food
+                service and add any dish he want to taste.
               </p>
               <div className="card-actions">
                 <button className="btn btn-primary">
@@ -57,7 +57,7 @@ const MyProjects = () => {
             </div>
           </div>
           {/* card-three */}
-          <div className="card w-96 mx-auto bg-base-100 shadow-xl">
+          <div className="card w-100 mx-auto bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
               <img
                 src="https://i.ibb.co/ZhtsdVg/fun-quiz-hub.png"
@@ -68,8 +68,8 @@ const MyProjects = () => {
             <div className="card-body items-center text-center">
               <h2 className="card-title text-3xl font-bold">Food review App</h2>
               <p>
-                This is a quiz app, here user can attend in quiz and see the
-                results.
+                This is a quiz app, user can participate in quiz fo selected
+                topic and see the results.
               </p>
               <div className="card-actions">
                 <button className="btn btn-primary">
